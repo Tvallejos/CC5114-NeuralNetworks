cddda952405e3a4a7d5adbea01e27e7909d7c608
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-12-06 13:25:10 -0300

    partial abstract node

--- GeneticAlgorithm/src/GP/Tree/INode.java
+++ GeneticAlgorithm/src/GP/Tree/INode.java
@@ -3,7 +3,9 @@ package GP.Tree;
 import java.util.ArrayList;
 
 public interface INode {
-    Double evaluate();
+    Double evaluate(Double x, Double y);
+
     ITree copy();
+
     ArrayList<INode> serialize();
 }
